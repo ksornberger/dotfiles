06d591510abada0a543e686b2edf976f6bfcc8b0
Kevin Sornberger <ksornberger@gmail.com>
2011-06-15 14:13:12 -0400

    Adding CoffeeDash SSH aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,6 @@ alias dotmate='mate $ZSH'
 alias dotcd='cd $ZSH'
 
 alias reload!='. ~/.zshrc'
+
+alias ssh-coffeedash-dev='ssh -L 9097:127.0.0.1:3306 rails@96.126.112.46'
+alias ssh-coffeedash-prod='ssh -L 9094:127.0.0.1:3306 rails@66.228.51.214'
