8bbe9fc9298e6f44f4ae4ed61baccb369c458867
Kevin Sornberger <ksornberger@gmail.com>
2012-05-03 14:51:30 -0400

    Updated Rakefile to work correctly based on @holman. Added todo.sh

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,7 @@
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export NODE_PATH="/usr/local/lib/node_modules"
+
+export DYLD_LIBRARY_PATH="/usr/local/mysql/lib:$DYLD_LIBRARY_PATH"
