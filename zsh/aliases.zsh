a6285306b7d9eac465a32e1c8e495240b3c015fa
Kevin Sornberger <kevin@devbbq.com>
2017-06-07 11:04:19 -0400

    Removing some unnecessary aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,10 +3,7 @@ alias dotcd='cd $ZSH'
 
 alias reload!='. ~/.zshrc'
 
-alias ssh-coffeedash-dev='ssh -L 9097:127.0.0.1:3306 rails@96.126.112.46'
-alias ssh-coffeedash-prod='ssh -L 9094:127.0.0.1:3306 rails@66.228.51.214'
 
 alias ssh-nzbzn='ssh -i nzbzn_key.pem ubuntu@nzbzn.com'
 alias ssh-pf='ssh -i ~/.ssh/present_feedback.pem -L 9099:127.0.0.1:27017 root@50.16.243.42'
-alias ssh-reallistings='ssh ksornberger@72.51.30.26'
 alias ssh-userclues='ssh root@66.228.49.98'
